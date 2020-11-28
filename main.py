9f0156770d1142dfbec7910d3f614bbdf2f0a128
DSRL <72488256+DefenceandSpaceRoboticsLaboratory@users.noreply.github.com>
2020-11-28 16:47:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,51 +3,96 @@ import os
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-in1 = 27
-in2 = 22
-in3 = 5
-in4 = 6
-light = 17
-fan = 26
+en1 = 17
+in1=27
+in2=22
+en2=10
+in3=9
+in4=11
+pwm1 = 13
+pwm2 =19
 
-GPIO.setup(fan, GPIO.OUT)
+kills=26
+uv1 = 3
+uv2 = 4
+dclight = 2
 
-GPIO.output(fan, GPIO.LOW)
 
-GPIO.setup(light, GPIO.OUT)
 
-GPIO.output(light, GPIO.LOW)
+GPIO.setup(uv1, GPIO.OUT)
+GPIO.setup(uv2, GPIO.OUT)
+GPIO.setup(kills, GPIO.OUT)
+GPIO.setup(dclight, GPIO.OUT)
+GPIO.output(dclight, GPIO.LOW)
+GPIO.output(uv1, GPIO.LOW)
+GPIO.output(uv2, GPIO.LOW)
 
+
+GPIO.setup(en1, GPIO.OUT)
+GPIO.setup(en2, GPIO.OUT)
+GPIO.setup(pwm1, GPIO.OUT)
+GPIO.setup(pwm2, GPIO.OUT)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
 GPIO.setup(in3, GPIO.OUT)
 GPIO.setup(in4, GPIO.OUT)
 root=""
 
+
+p = GPIO.PWM(19,100)          #GPIO19 as PWM output, with 100Hz frequency
+p.start(0)                              #generate PWM signal with 0% duty cycle
+q = GPIO.PWM(13,100)
+q.start(0)
+spd=0
+
+
+
+def speed_inc():    ##if uv on hoi speed 10  korbo lagbo
+    global spd
+    spd = spd + 10
+    if(spd >75):
+        spd = 75
+    p.ChangeDutyCycle(spd)
+    
+def speed_dec():
+    global spd
+    spd = spd - 10
+    if(spd < 0):
+        spd = 0
+    p.ChangeDutyCycle(spd)
+    
+
+def move_forward():
+    GPIO.output(en1,GPIO.HIGH)
+    GPIO.output(en2,GPIO.HIGH)
+    speed_inc()
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)                           #
+    print('moving forward')
+
 def stop():
+    GPIO.output(en1,GPIO.HIGH)
+    GPIO.output(en2,GPIO.HIGH)
+    speed_dec()
     GPIO.output(in1,GPIO.LOW)
     GPIO.output(in2,GPIO.LOW)
     GPIO.output(in3,GPIO.LOW)
     GPIO.output(in4,GPIO.LOW)
     print('motor stop')
-
-myfile="/home/pi/UvRobot/forward.text"
-
-def move_forward():
-    print('moving forward')
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
-    GPIO.output(in3,GPIO.HIGH)
-    GPIO.output(in4,GPIO.LOW)
-   
     
 def turn_left():
+    GPIO.output(en1,GPIO.HIGH)
+    GPIO.output(en2,GPIO.HIGH)
     GPIO.output(in1,GPIO.LOW)
     GPIO.output(in2,GPIO.LOW)
     GPIO.output(in3,GPIO.HIGH)
     GPIO.output(in4,GPIO.LOW)
 
 def turn_right():
+    GPIO.output(en1,GPIO.HIGH)
+    GPIO.output(en2,GPIO.HIGH)
     GPIO.output(in1,GPIO.HIGH)
     GPIO.output(in2,GPIO.LOW)
     GPIO.output(in3,GPIO.LOW)
@@ -56,6 +101,8 @@ def turn_right():
 
 
 def move_backward():
+    GPIO.output(en1,GPIO.HIGH)
+    GPIO.output(en2,GPIO.HIGH)
     GPIO.output(in1,GPIO.LOW)
     GPIO.output(in2,GPIO.HIGH)
     GPIO.output(in3,GPIO.LOW)
@@ -68,7 +115,7 @@ while True:
     path=root+"temp/for.rob"
     if os.path.isfile(path):
         for i in range(5):
-            if (os.path.isfile(path)) and (not os.path.isfile("temp/us1.rob")) and (not os.path.isfile("temp/us2.rob")) and (not os.path.isfile("temp/us3.rob")):
+            if (os.path.isfile(path)) and (not os.path.isfile("temp/us1.rob")) and (not os.path.isfile("temp/us2.rob")) and (not os.path.isfile("temp/us3.rob") and (not os.path.isfile("temp/us4.rob"))):
                 move_forward()
                 time.sleep(.5)
             else:
@@ -133,13 +180,15 @@ while True:
 
                     
     path=root+"temp/uv.rob"
-    if os.path.isfile(path) and (not os.path.isfile("temp/pir1.rob")) and (not os.path.isfile("temp/pir2.rob")) and (not os.path.isfile("temp/pir3.rob")):
-        GPIO.output(light,GPIO.HIGH)
+    if os.path.isfile(path) and (not os.path.isfile("temp/pir1.rob")) and (not os.path.isfile("temp/pir2.rob")) and (not os.path.isfile("temp/pir3.rob")) and (not os.path.isfile("temp/pir4.rob")):
+        GPIO.output(uv1,GPIO.HIGH)
+        GPIO.output(uv2,GPIO.HIGH)
     else:
-        GPIO.output(light,GPIO.LOW)
+        GPIO.output(uv1,GPIO.LOW)
+        GPIO.output(uv2,GPIO.LOW)
 
                     
-    print('fan on')
+    '''print('fan on')
                     
     path=root+"temp/fan.rob"
     if os.path.isfile(path):
@@ -147,7 +196,8 @@ while True:
         print('fan if')
     else:
         GPIO.output(fan,GPIO.LOW)
-        print('fan else')
+        print('fan else')'''
                     
+
 
 

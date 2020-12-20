f499153511c4eb62275fe611de44b03b32a3201d
KishalayChakraborty <kishalaychakraborty1@gmail.com>
2020-12-20 21:39:32 +0530

    Update and rename arduno.ino to ardunoV2Relay.ino

--- working/arduno/ardunoV2Relay.ino
+++ working/arduno/ardunoV2Relay.ino
@@ -1,36 +1,29 @@
 //-----------------I2C -----------------
 
 //-----------------M1------------------
-#define en1 2
-#define in1 3
-#define in2 4
-#define pwm1 5
+#define in1 2
+#define in2 3
 //----------------M2--------------------
-#define en2 6
-#define in3 7 //pankhi check connection for this 
-#define in4 8
-#define pwm2 9
-//-----------------RELAY----------------
+#define in3 4 
+#define in4 5
+//-----------------UV----------------
+#define uv1 8  //UV
+#define uv2 9  //UV
+//-----------------LED----------------
 #define green 10 //GREEEN LIGHT
 #define red 11 //RED N OTHR DC LIGHTS 
-#define uv1 13  //UV
-#define uv2 12  //UV
-int speed1 = 0;
-int speed2 = 0;
-int inc = 5;
+
+
+//int speed1 = 0;
+//int speed2 = 0;
+
 
 //------------I2C Function----------
 void receiveData(char b,char a,char c)//a=speed,b=direction,c=uv status
 {
   int fl=0;
   int fl2=0;
-  {   {
-          speed1=(int)(a- '0')*25;
-          speed2=speed1;
-          Serial.print("Speed :");
-          Serial.print(speed1);
-          Serial.println(":");
-      }
+  { 
       {
           
           if (b=='0'){
@@ -81,39 +74,26 @@ void receiveData(char b,char a,char c)//a=speed,b=direction,c=uv status
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
-    pinMode(en1,OUTPUT);
     pinMode(in1,OUTPUT);
     pinMode(in2,OUTPUT);
-    pinMode(pwm1,OUTPUT);
-    pinMode(en2,OUTPUT);
     pinMode(in3,OUTPUT);
     pinMode(in4,OUTPUT);
-    pinMode(pwm2,OUTPUT);
     pinMode(green,OUTPUT);
     pinMode(red,OUTPUT);
     pinMode(uv2,OUTPUT);
     pinMode(uv1,OUTPUT);
-    speed1=0;
-    speed2=0;
     digitalWrite(green,LOW);
     digitalWrite(red,LOW);
     digitalWrite(uv1,LOW);
     digitalWrite(uv2,LOW);
-    digitalWrite(en1,LOW);
-    digitalWrite(en2,LOW);
     digitalWrite(in1,LOW);
     digitalWrite(in2,LOW);
     digitalWrite(in3,LOW);
     digitalWrite(in4,LOW);
-    analogWrite(pwm1, 0);
-    analogWrite(pwm2, 0);
 
 }
 
 void forward(){
-    updatespeed();
-    digitalWrite(en1,HIGH);
-    digitalWrite(en2,HIGH);
     digitalWrite(in1,HIGH);
     digitalWrite(in2,LOW);
     digitalWrite(in3,HIGH);
@@ -122,9 +102,6 @@ void forward(){
     Serial.println("forward");
 }
 void backward(){
-    updatespeed();
-    digitalWrite(en1,HIGH);
-    digitalWrite(en2,HIGH);
     digitalWrite(in2,HIGH);
     digitalWrite(in1,LOW);
     digitalWrite(in4,HIGH);
@@ -133,9 +110,6 @@ void backward(){
     Serial.println("backward");
 }
 void left(){
-    updatespeed();
-    digitalWrite(en1,HIGH);
-    digitalWrite(en2,HIGH);
     digitalWrite(in1,HIGH);
     digitalWrite(in2,LOW);
     digitalWrite(in4,HIGH);
@@ -143,9 +117,6 @@ void left(){
     Serial.println("left");
 }
 void right(){
-    updatespeed();
-    digitalWrite(en1,HIGH);
-    digitalWrite(en2,HIGH);
     digitalWrite(in2,HIGH);
     digitalWrite(in1,LOW);
     digitalWrite(in3,HIGH);
@@ -153,24 +124,11 @@ void right(){
     Serial.println("right");
 }
 void stop(){
-    speed1=0;
-    speed2=0;
-    updatespeed();
-    digitalWrite(en1,HIGH);
-    digitalWrite(en2,HIGH);
     digitalWrite(in1,LOW);
     digitalWrite(in2,LOW);
     digitalWrite(in3,LOW);
     digitalWrite(in4,LOW);
     Serial.println("stop");
-}
-void updatespeed(){
-    analogWrite(pwm1, speed1);
-    analogWrite(pwm2, speed2);    
-    Serial.print("Speed:");  
-    Serial.print(speed1); 
-    Serial.print(",");
-    Serial.println(speed2);
 }
 
 
